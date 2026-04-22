6c5180b2803fb69a0ab0d80d7cb49c084c6de566
cwha0212 <cwha02120212@gmail.com>
2026-04-22 19:02:43 +0900

    6

--- systemd/start.sh
+++ systemd/start.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 source /opt/ros/humble/setup.bash
 source /home/maum/last_navi/install/setup.bash
+
+export ROS_DOMAIN_ID=30
+export PYTHONUNBUFFERED=1
+export LD_LIBRARY_PATH=/home/maum/local/gtsam-4.1.1/lib:/usr/local/lib:/usr/lib/x86_64-linux-gnu
+export PATH=/usr/local/bin:/usr/bin:/bin
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export ROS_LOCALHOST_ONLY=0
+
 python3 -u /home/maum/last_navi/src/LIGO./scripts/mqtt_mode_orchestrator.py
