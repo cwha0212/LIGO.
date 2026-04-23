a26dcb818b377ec73e8557f8ec0f1572d72d9164
cwha0212 <cwha02120212@gmail.com>
2026-04-22 22:29:38 -0400

    systemctl done

--- systemd/start.sh
+++ systemd/start.sh
@@ -4,8 +4,7 @@ source /home/maum/last_navi/install/setup.bash
 
 export ROS_DOMAIN_ID=30
 export PYTHONUNBUFFERED=1
-export LD_LIBRARY_PATH=/home/maum/local/gtsam-4.1.1/lib:/usr/local/lib:/usr/lib/x86_64-linux-gnu
-export PATH=/usr/local/bin:/usr/bin:/bin
+export LD_LIBRARY_PATH="/home/maum/local/gtsam-4.1.1/lib:/usr/local/lib:${LD_LIBRARY_PATH}"
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export ROS_LOCALHOST_ONLY=0
 
