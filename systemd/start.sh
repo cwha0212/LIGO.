bd835153e647282e60bd07d5a539a1c7e5da9fdc
cwha0212 <cwha02120212@gmail.com>
2026-04-23 05:18:23 -0400

    sh file

--- systemd/start.sh
+++ systemd/start.sh
@@ -6,6 +6,7 @@ export ROS_DOMAIN_ID=30
 export PYTHONUNBUFFERED=1
 export LD_LIBRARY_PATH="/home/maum/local/gtsam-4.1.1/lib:/usr/local/lib:${LD_LIBRARY_PATH}"
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CYCLONEDDS_URI=file:///etc/ros/cyclonedds.xml
 export ROS_LOCALHOST_ONLY=0
 
 python3 -u /home/maum/last_navi/src/LIGO./scripts/mqtt_mode_orchestrator.py
